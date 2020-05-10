10e47b634e04598493b14ac78e91ae948d50a6f5
Sebastien Lemery <seblemery@gmail.com>
2020-05-09 23:43:38 -0400

    Update c_main.lua

--- fivem_script/tokovoip_script/src/c_main.lua
+++ fivem_script/tokovoip_script/src/c_main.lua
@@ -17,7 +17,7 @@
 local targetPed;
 local useLocalPed = true;
 local isRunning = false;
-local scriptVersion = "1.3.5";
+local scriptVersion = "2.0.0";
 local animStates = {}
 local displayingPluginScreen = false;
 local HeadBone = 0x796e;
